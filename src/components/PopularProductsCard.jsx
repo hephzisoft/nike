533b1deb9fde910577e341e3ee0d48b65ad7f7de
Hephzisoft <hephzisoft@gmail.com>
2023-12-17 10:26:33 +0100

    chn

--- src/components/PopularProductsCard.jsx
+++ src/components/PopularProductsCard.jsx
@@ -2,12 +2,16 @@ import { star } from "../assets/icons"
 
 const PopularProductsCard = ({ imgURL, name, price}) => {
   return (
-    <div className="flex flex-1 w-full max-sm:w-full">
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
 
         <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
-        <div className="mt-[8rem] flex flex-col justify-start gap-2.5">
+        <div className="mt-8 flex flex-row justify-start gap-2.5">
             <img src={star} alt="rating" width={24} height={24} className=""/>
-        </div>
+            <p className=" font-montserrat text-slate-gray leading-normal text-xl">(4.5)</p>
+        
+        </div>  
+      <h3 className="leading-noraml mt-2 font-semibold font-palanquin text-2xl">{name}</h3>
+      <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">{price}</p>
     </div>
   )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,23 @@
 
+import Button from '../components/Button'
+import { headerLogo } from '../assets/images'
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex jsutify-between items-center max-lg:flex-col gap-10 w-full max-container">
+
+      <div className="flex flex-1 flex-col">
+      <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <h2 className=" capitalize font-palanquin text-4xl font-bold"><span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <br />
+          <span className="inline-block text-coral-red mt-3">Nike </span> Shoes
+        </h2>
+        .ss
+    
+     
+      <Button label='View' />
+      </div>
+    </section>
   )
 }
 
